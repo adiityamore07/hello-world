5564125250a39a31860b12b840cd17772816a12f
Aditya  More <154891883+adiityamore07@users.noreply.github.com>
2026-04-25 12:17:25 +0530

    new

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.end("Hello World from CI/CD 🚀");
+    fs.readFile('index.html', (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 });
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+server.listen(3000);
